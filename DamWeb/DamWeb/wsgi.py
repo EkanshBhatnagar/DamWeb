d0b22e8ba2630173956a37f257bb9cb050bb134c
EkanshBhatnagar <ekanshbhatnagar@outlook.com>
2020-02-19 19:53:07 +0530

    edited

--- DamWeb/DamWeb/wsgi.py
+++ DamWeb/DamWeb/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DamWeb.settings')
 
 application = get_wsgi_application()
+
+
+DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+sys.path.append(DJANGO_PATH)
