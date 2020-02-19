51615db6d9e6652245e375d6ae95405cec06f352
EkanshBhatnagar <ekanshbhatnagar@outlook.com>
2020-02-19 20:19:27 +0530

    added templates

--- DamWeb/main/views.py
+++ DamWeb/main/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request, 'main/main.html')
 
 
 def about(request):
