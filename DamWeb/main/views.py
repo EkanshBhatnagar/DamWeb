88ab7214329a5e687dfa16e309c9a04fa35e973b
EkanshBhatnagar <ekanshbhatnagar@outlook.com>
2020-02-19 20:30:54 +0530

    added functionality of post for testing

--- DamWeb/main/views.py
+++ DamWeb/main/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'Author': 'Ekansh',
+        'Title': 'Pressure Meter',
+        'Content': '102002 psi',
+        'data': 'Feb 19 2020',
+    },
+    {
+        'Author':'Ekansh1',
+        'Title':'Pressure1 Meter',
+        'Content':'1020021 psi',
+        'data':'Feb 19 20120',
+    }
+
+]
+
 
 def home(request):
-    return render(request, 'main/main.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'main/main.html',context)
 
 
 def about(request):
